840fdc70adfb16eda24a810b9fad2dcd26704b0a
donkomura <koiru3822fs@gmail.com>
2022-07-08 23:03:03 +0900

    id length

--- sql/01_schema.sql
+++ sql/01_schema.sql
@@ -2,7 +2,7 @@ USE `isucon2021_prior`;
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-  `id`         VARCHAR(255) PRIMARY KEY NOT NULL,
+  `id`         VARCHAR(20) PRIMARY KEY NOT NULL,
   `email`      VARCHAR(255) NOT NULL DEFAULT '',
   `nickname`   VARCHAR(120) NOT NULL DEFAULT '',
   `staff`      BOOLEAN NOT NULL DEFAULT false,
@@ -12,7 +12,7 @@ CREATE TABLE `users` (
 
 DROP TABLE IF EXISTS `schedules`;
 CREATE TABLE `schedules` (
-  `id`         VARCHAR(255) PRIMARY KEY NOT NULL,
+  `id`         VARCHAR(20) PRIMARY KEY NOT NULL,
   `title`      VARCHAR(255) NOT NULL DEFAULT '',
   `capacity`   INT UNSIGNED NOT NULL DEFAULT 0,
   `created_at` DATETIME(6) NOT NULL,
@@ -22,7 +22,7 @@ CREATE TABLE `schedules` (
 
 DROP TABLE IF EXISTS `reservations`;
 CREATE TABLE `reservations` (
-  `id`          VARCHAR(255) PRIMARY KEY NOT NULL,
+  `id`          VARCHAR(20) PRIMARY KEY NOT NULL,
   `schedule_id` VARCHAR(255) NOT NULL,
   `user_id`     VARCHAR(255) NOT NULL,
   `created_at`  DATETIME(6) NOT NULL
